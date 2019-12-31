e1b5abd7ce3350481678d1930e1141f04229b6f4
Andrew Zhang <zhangmhao@gmail.com>
2020-01-01 00:51:50 +0800

    feat:  extract info from rss feed

--- build/createList.js
+++ build/createList.js
@@ -1,29 +1,145 @@
-const fs = require('fs');
-const path = require('path');
-const toml = require('toml');
+const xml2js = require('xml2js');
+const ora = require('ora');
+const chalk = require('chalk');
 const utils = require('./utils');
 
-
-
+const findCategory = (categories, id) => categories.find((cate) => cate.id === id);
 
 async function readRSS(rssUrl) {
   return utils.getUrl(rssUrl);
 }
+const get$Val = (v, key) => v.$ && v.$[key];
+const pickVal = (obj, keyList) => {
+  return keyList.reduce(
+    (result, key) => {
+      let pair = key;
+      if (typeof key === 'string') {
+        pair = { from: key, to: key };
+      }
+      return { ...result, [pair.to]: obj[pair.from] };
+    },
+    {},
+  );
+};
+
+function getAverageDuration(episodes) {
+  if (!episodes || episodes.length === 0) {
+    return null;
+  }
+  const getSeconds = (str) => {
+    let s = 0;
+    if (str.indexOf(':') >= 0) {
+      const p = str.split(':');
+      let m = 1;
+      while (p.length > 0) {
+        s += m * parseInt(p.pop(), 10);
+        m *= 60;
+      }
+    } else {
+      s = parseInt(str, 10);
+    }
+    return s;
+  };
+  const durations = episodes
+    .map((episode) => {
+      const durationString = episode['itunes:duration'];
+      return getSeconds(durationString) || 0;
+    })
+    .filter((val) => val > 0)
+    .sort((a, b) => b - a);
+
+  return Math.round(durations.reduce((total, val) => total + val, 0) / durations.length);
+}
+function extractBasicInfo(rssObj) {
+  let basicInfo = {};
+  const { channel } = rssObj;
+  if (channel) {
+    const allEpisode = channel.item || [];
+    const latestEpisode = allEpisode[0];
+    basicInfo = {
+      ...basicInfo,
+      ...pickVal(channel, [
+        'title', 'link', 'description', 'language',
+        { from: 'itunes:keywords', to: 'keywords' },
+        { from: 'itunes:author', to: 'author' },
+      ]),
+      ituneCategory: get$Val(channel['itunes:category'], 'text'),
+      image: get$Val(channel['itunes:image'], 'href'),
+      episodeNumber: allEpisode.length,
+      averageDuration: getAverageDuration(allEpisode),
+      latestEpisode,
+    };
+  }
+  return basicInfo;
+}
+
 
 async function mainProcess() {
-  const content = utils.readTomlData('./data/content.toml');
-  const { podcasts } = content;
-  for (let i = 0; i < podcasts.length; i++) {
+  const content = utils.readTomlData('./config/content.toml');
+  const { podcasts = [], categories } = content;
+  const failed = [];
+  for (let i = 0; i < podcasts.length; i += 1) {
     const podcast = podcasts[i];
-    const rssXml = await readRSS(podcast.rss);
-    console.log(rssXml);
+    const podcastCategoryIds = podcast.cate.split(',');
+    const fullCategories = podcastCategoryIds.map((cateId) => findCategory(categories, cateId));
+    const prefix = (text) => `${chalk.bold(`[${podcast.name}]`)} ${text}`;
+
+    // read rss content from rss feed
+    const spinner = ora(prefix('loading RSS')).start();
+    let rssResult;
+    let rssXml;
+    try {
+      rssResult = await readRSS(podcast.rss);
+      rssXml = rssResult.data;
+    } catch (err) {
+      failed.push(podcast);
+      spinner.fail(prefix(chalk.red(`Read From RSS feed Failed. Detail: ${err.message}`)));
+      // eslint-disable-next-line no-continue
+      continue;
+    }
+
+
+    // parsing xml to json
+    let jsonObj;
+    spinner.text = prefix('Parsing RSS xml file to json');
+    try {
+      const originRssObj = await xml2js.parseStringPromise(rssXml, { explicitArray: false });
+      jsonObj = {
+        lastUpdated: Date.now(),
+        categories: fullCategories,
+        basicInfo: extractBasicInfo(originRssObj.rss),
+        originRssObj,
+      };
+    } catch (err) {
+      failed.push(podcast);
+      spinner.fail(prefix(chalk.red(`Parsing RSS xml feed Failed. Detail: ${err.message}`)));
+      continue;
+    }
+
+    try {
+      // writing to local file system
+      spinner.text = prefix('writeing RSS xml file');
+      await utils.saveRSSToLocalFile(`${podcast.name}.xml`, rssXml);
+      await utils.saveRSSToLocalFile(`${podcast.name}.json`, JSON.stringify(jsonObj, null, 2));
+    } catch (err) {
+      failed.push(podcast);
+      spinner.fail(prefix(chalk.red(`Write to local file failed. Detail: ${err.message}`)));
+      continue;
+    }
+
+    spinner.succeed(prefix('updated'));
   }
+  return { podcasts, failed };
 }
 
 mainProcess()
-.then(() => {
-  console.log('done!');
-})
-.catch((err) => {
-  console.error(err);
-});
+  .then((result) => {
+    console.log(`
+  ${chalk.green.bold('Success')}: ${result.podcasts.length - result.failed.length}
+  ${chalk.red.bold('Failed')}: ${result.failed.length}
+    `);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(-1);
+  });
--- build/utils.js
+++ build/utils.js
@@ -1,9 +1,13 @@
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
 const toml = require('toml');
 const axios = require('axios');
 
 const currentWorkDirectory = process.cwd();
+const rssDir = path.resolve(currentWorkDirectory, './rss');
+const writeFilePromise = util.promisify(fs.writeFile);
+
 
 function readTomlData(filePath) {
   const dataPath = path.resolve(currentWorkDirectory, filePath);
@@ -15,7 +19,13 @@ function getUrl(url) {
   return axios.get(url);
 }
 
+function saveRSSToLocalFile(filename, content) {
+  const writePath = path.resolve(rssDir, filename);
+  return writeFilePromise(writePath, content);
+}
+
 module.exports = {
   readTomlData,
   getUrl,
+  saveRSSToLocalFile,
 };
